9b7b7dc08778c4206aadb122126e238dbe1f20b0
Paulius <pauliuskaminskas95@gmail.com>
2025-08-09 15:18:55 +0300

    clock update

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 const dateEl = document.getElementsByClassName('date')[0];
 const timeEl = document.getElementsByClassName('time')[0];
+const hourEl = document.getElementsByClassName('hours')[0];
+const minsEl = document.getElementsByClassName('minutes')[0];
+const secsEl = document.getElementsByClassName('seconds')[0];
+
+const weekdays = [
+    'Sekmadienis',
+    'Pirmadienis',
+    'Antradienis',
+    'Trečiadienis',
+    'Ketvirtadienis',
+    'Penktadienis',
+    'Šeštadienis',
+];
+const months = [
+    'Sausio',
+    'Vasario',
+    'Kovo',
+    'Balandžio',
+    'Gegužės',
+    'Birželio',
+    'Liepos',
+    'Rugpjūčio',
+    'Rugsėjo',
+    'Spalio',
+    'Lapkričio',
+    'Gruodžio',
+];
 
 function clock() {
     const now = new Date();
@@ -10,17 +37,17 @@ function clock() {
     hours = addZero(hours);
     minutes = addZero(minutes);
     seconds = addZero(seconds);
+    const month = now.getMonth();
+    const weekday = now.getDay();
+    const day = now.getDate();
 
     timeEl.textContent = hours + ':' + minutes + ':' + seconds;
+    dateEl.textContent =
+        weekdays[weekday] + ', ' + months[month] + ' ' + day + 'd.';
 
-    let day = now.getDate();
-    let month = now.getMonth() + 1;
-    let year = now.getFullYear();
-    day = addZero(day);
-    month = addZero(month);
-    year = addZero(year);
-    dateEl.textContent = `${day}/${month}/${year}`;
-
+    hourEl.style.transform = 'rotate(' + hours * 30 + 'deg)';
+    minsEl.style.transform = 'rotate(' + minutes * 6 + 'deg)';
+    secsEl.style.transform = 'rotate(' + seconds * 6 + 'deg)';
     setTimeout(clock, 1000);
 }
 function addZero(t) {
